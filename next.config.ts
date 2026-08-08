221f0f96174a6a11a489b5e3f50d0aabfaedc04f
Vaibhav garg <vaibhav.garg@ohiapp.com>
2026-08-09 00:48:10 +0530

    Update next.config.ts for GitHub Pages deployment and adjust TypeScript references in next-env.d.ts; add GitHub Actions workflow for deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
+const repo = "ickdeath";
+const isGithubPages = process.env.GITHUB_PAGES === "true";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: isGithubPages ? `/${repo}` : undefined,
+  assetPrefix: isGithubPages ? `/${repo}/` : undefined,
+  images: {
+    unoptimized: true,
+  },
+  trailingSlash: true,
 };
 
 export default nextConfig;
